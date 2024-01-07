c02b668b599d9052075f44606cf413261fbbe448
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2024-01-07 15:17:19 +0530

    Todo-List

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = ({length}) => {
+    const year = new Date();
+
+  return (
+    <footer>
+      {length} List {length === 1 ? "item" : "items"}<br/>
+     Copyright &copy; {year.getFullYear()}
+    </footer>
+  )
+}
+
+export default Footer
